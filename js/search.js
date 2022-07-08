b21e6466b8aa35620402446e076a1c4d8cb1c2ee
ptqiu <ptqiu@foxmail.com>
2022-07-08 17:12:04 +0800

    init data

--- js/search.js
+++ js/search.js
@@ -1,18 +1,10 @@
 const sInfor = document.getElementById("search");
-const sBaidu = document.getElementById("baidu");
+const sFsearch = document.getElementById("Fsearch");
 const sGoogle = document.getElementById("google");
 const sBing = document.getElementById("bing");
+const sBaidu = document.getElementById("baidu");
 const sGitHub = document.getElementById("github");
-const sGitee = document.getElementById("gitee");
-const sZhihu = document.getElementById('zhihu');
-const sJd = document.getElementById('jd');
-const sTaoBao = document.getElementById('taobao');
-const sYouTube = document.getElementById('youtube')
-const sBilibili = document.getElementById("bilibili");
-const sYingshi = document.getElementById("yingshi");
-const sWangpan = document.getElementById("wangpan");
-const sBt = document.getElementById("bt");
-const sFanyi = document.getElementById("fanyi");
+const sniceso = document.getElementById("niceso");
 
 
 //isEmpty;
@@ -54,7 +46,16 @@ let doAction=function(p,n,s){
 //拼接网址
 let p='';
 let n='';
-let s=''
+let s='';
+
+//Fsearch
+sFsearch.onclick=function(){
+    p="https://fsoufsou.com/search?q=";
+    n="&tbn=all";
+    s="https://fsoufsou.com/";
+ doAction(p,n,s);
+
+}
 
 //Google
 sGoogle.onclick=function(){
@@ -65,6 +66,14 @@ sGoogle.onclick=function(){
     s='https://www.google.com/webhp?gws_rd=cr&nfpr=1&newwindow=1&num=30';
    doAction(p,n,s);
 
+}
+
+//Bing
+sBing.onclick=function () {
+    p='https://cn.bing.com/search?q=';
+    n='&ensearch=1&FORM=BESBTB'
+    s='https://www4.bing.com/?scope=web&FORM=BESBTB';
+    doAction(p,n,s);
 }
 
 //BaiDu
@@ -84,13 +93,7 @@ document.onkeydown = function (e) { // 回车提交表单
             };
         }
     
-//Bing
-sBing.onclick=function () {
-    p='https://cn.bing.com/search?q=';
-    n='&ensearch=1&FORM=BESBTB'
-    s='https://www4.bing.com/?scope=web&FORM=BESBTB';
-    doAction(p,n,s);
-}
+
 
 //GitHub
 sGitHub.onclick= function () {
@@ -100,86 +103,17 @@ sGitHub.onclick= function () {
     doAction(p,n,s);
 }
 
-//Gitee
-sGitee.onclick = function () {
-    p='https://gitee.com/search?utf8=✓&search=';
-    n='';
-    s='https://gitee.com/'
-    doAction(p,n,s);
+//Niceso
+sniceso.onclick = function () {
+    p='https://www.niceso.fun/search/?q=';
+    s='https://www.niceso.fun/';
+    doAction(p,'',p);
 }
 
-//Zhihu
-sZhihu.onclick= function () {
-    p='https://www.zhihu.com/search?q=';
-    n='&type=content&utm_content=search_hot';
-    s='https://www.zhihu.com/'
-    doAction(p,n,s);
-}
-//jd
-sJd.onclick = function () {
-    p='https://search.jd.com/Search?enc=utf-8&keyword=';
-    n='';
-    s='https://search.jd.com/'
-    doAction(p,n,s);
-}
-//TaoBao
-sTaoBao.onclick =function () {
-    p='https://s.taobao.com/search?q=';
-    n='&taoke_type=1';
-    s='https://s.taobao.com/';
-    doAction(p,n,s);
-}
-
-//YouTube
-sYouTube.onclick =function () {
-    p='https://www.youtube.com/results?search_query=';
-    doAction(p,n,p);
-}
-
-//Bilibili
-sBilibili.onclick =function () {
-    p='https://search.bilibili.com/all?keyword=';
-    n='';
-    s='https://search.bilibili.com'
-    doAction(p,n,s);
-}
-
-//YingShi
-sYingshi.onclick = function () {
-    p='http://ifkdy.com/?q=';
-    n='&p=1';
-    s='http://ifkdy.com/';
-    doAction(p,n,s);
-}
-
-//Wangpan
-sWangpan.onclick = function () {
-    p='http://www.panuso.com/s/';
-    n='.html';
-    s='http://www.panuso.com'
-    doAction(p,n,s);
-}
-
-//BT
-sBt.onclick = function () {
-    p='https://www.bturl.cc/search/';
-    n='_ctime_1.html';
-    s='https://www.bturl.cc'
-    doAction(p,n,s);
-}
-
-//翻译
-sFanyi.onclick = function (){
-    p='https://fanyi.baidu.com/#auto/zh/';
-    n='';
-    s='https://fanyi.baidu.com/';
-    doAction(p,n,s);
-}
 console.log("                                  ");
-console.log("   - 欢迎来到Morey的个人导航 -     ");
+console.log("   - 欢迎来到小邱鱼的个人导航 -     ");
 console.log("===============================");
 console.log("                          ");
 console.log("       老哥，你好啊~      ");
 console.log("                          ");
 console.log("===============================")
-
